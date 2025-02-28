b0506388f988330d1c152744318ca73761da6796
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2025-02-28 22:23:18 +0800

    added Sumamry boxes on dashboard

---
+++ resources/js/Components/SummaryBox.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+defineProps({
+    heading: String,
+    value: {
+        required: true,
+    },
+    subheading: String,
+    icon: {
+        required: true,
+    },
+});
+</script>
+
+<template>
+    <Card>
+        <CardHeader
+            class="flex flex-row items-center justify-between space-y-0 pb-2"
+        >
+            <CardTitle class="text-sm font-medium"> {{ heading }} </CardTitle>
+            <component :is="icon" class="h-4 w-4 text-muted-foreground" />
+        </CardHeader>
+        <CardContent>
+            <div class="text-2xl font-bold">{{ value }}</div>
+            <p class="text-xs text-muted-foreground">{{ subheading }}</p>
+        </CardContent>
+    </Card>
+</template>
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,5 +1,25 @@
-<script setup></script>
+<script setup>
+import SummaryBox from "@/Components/SummaryBox.vue";
+</script>
 
 <template>
-    <MainLayout> </MainLayout>
+    <MainLayout>
+        <section class="grid grid-cols-3 gap-10">
+            <SummaryBox
+                heading="Total Employees"
+                value="10"
+                subheading="Total number of employees"
+            />
+            <SummaryBox
+                heading="Total Users"
+                value="10"
+                subheading="Total number of users"
+            />
+            <SummaryBox
+                heading="Total Documents"
+                value="10"
+                subheading="Total number of documents"
+            />
+        </section>
+    </MainLayout>
 </template>
