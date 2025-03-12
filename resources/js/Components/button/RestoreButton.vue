92e4881b67623f73f5a24f0a4c69ed012fdc735e
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2025-03-12 08:47:09 +0800

    fixed the layout issue on documents archive page

--- resources/js/Components/button/RestoreButton.vue
+++ resources/js/Components/button/RestoreButton.vue
@@ -1,6 +1,6 @@
 <template>
     <button
-        class="cursor-pointer text-blue-500 sm:mr-3 size-5 hover:underline"
+        class="cursor-pointer text-blue-500 sm:mr-3 hover:underline"
         variant="link"
     >
         Restore
