0c7a0184af3352888043ceb70438c9edb23e2961
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2025-03-17 11:46:40 +0800

    added upload documents modal on documents index page

--- resources/js/Pages/Document/Index.vue
+++ resources/js/Pages/Document/Index.vue
@@ -1,6 +1,10 @@
 <script setup>
 import useDelete from "@/Composables/useDelete.js";
 import { useSearch } from "@/Composables/useSearch";
+import { useForm } from "@inertiajs/vue3";
+import { ref  } from "vue";
+import FileUpload from "primevue/fileupload";
+import Dialog from "primevue/dialog";
 defineProps({
     documents: {
         type: Object,
@@ -12,6 +16,50 @@ const { deleteModel } = useDelete("Document");
 const { search } = useSearch("documents.index");
 const getFileUrl = (path) => {
     return `/storage/${path}`;
+};
+
+import { useToast } from "primevue/usetoast";
+import Loading from "@/Components/Loading.vue";
+const isLoading = ref(false);
+const toast = useToast();
+
+const form = useForm({
+    documents: [],
+    employee_id: null,
+});
+const isUploadDocumentModalVisible = ref(false);
+const openUploadDocumentModal = (id) => {
+    form.employee_id = id;
+    isUploadDocumentModalVisible.value = true;
+};
+
+const onAdvancedUpload = (event) => {
+    isLoading.value = true;
+    form.documents = event.files;
+    form.post(route("documents.store-documents"), {
+        onSuccess: () => {
+            toast.add({
+                severity: "info",
+                summary: "Success",
+                detail: "Documents Uploaded",
+                life: 3000,
+            });
+            form.reset();
+            form.clearErrors();
+            isLoading.value = false;
+            isUploadDocumentModalVisible.value = false;
+        },
+        onError: (e) => {
+            console.log(e);
+            toast.add({
+                severity: "error",
+                summary: "Error",
+                detail: "An error occured while trying to upload the documents. Please make sure that the documents are on type PDF.",
+                life: 3000,
+            });
+            isLoading.value = false;
+        },
+    });
 };
 </script>
 
@@ -47,6 +95,10 @@ const getFileUrl = (path) => {
                         <TD>{{ document.person_indicated }}</TD>
                         <TD>{{ document.description ?? "None" }}</TD>
                         <TD class="flex flex-center gap-3">
+                            <UploadButton
+                                @click="openUploadDocumentModal(document.id)"
+                                >Upload Document</UploadButton
+                            >
                             <ShowButton
                                 :isLink="true"
                                 :href="route('documents.show', document.id)"
@@ -68,5 +120,38 @@ const getFileUrl = (path) => {
             </Table>
             <Pagination :data="documents" />
         </TableContainer>
+
+        <Dialog
+            v-model:visible="isUploadDocumentModalVisible"
+            modal
+            header="Edit Profile"
+            :style="{ width: '50rem' }"
+        >
+            <template #header>Upload Document</template>
+
+            <FormInput
+                label=""
+                :isRequired="false"
+                :errorMessage="form.errors.document"
+            >
+                <FileUpload
+                    name="documents"
+                    :custom-upload="true"
+                    @uploader="onAdvancedUpload($event)"
+                    :multiple="true"
+                    :maxFileSize="1000000"
+                    :headers="{
+                        'X-CSRF-TOKEN': $page.props.csrf_token,
+                    }"
+                >
+                    <template #empty>
+                        <span>Drag and drop files to here to upload.</span>
+                    </template>
+                </FileUpload>
+            </FormInput>
+            <Label v-if="isLoading" class="flex gap-1 items-center"
+                >Uploading <Loading />
+            </Label>
+        </Dialog>
     </MainLayout>
 </template>
