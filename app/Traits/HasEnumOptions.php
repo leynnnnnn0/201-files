bed0bf7ac1bb9c9b483e32053ab134cefb1dade8
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2025-04-20 10:02:25 +0800

    made the cos/jop uppercase on select options

--- app/Traits/HasEnumOptions.php
+++ app/Traits/HasEnumOptions.php
@@ -10,6 +10,7 @@ trait HasEnumOptions
     {
         $data = array_column(self::cases(), 'value');
         $values = collect($data)->map(function ($item) {
+            if (str_contains('cos/jop', $item)) return Str::upper($item);
             return Str::title($item);
         });
         return array_combine($data, $values->toArray());
