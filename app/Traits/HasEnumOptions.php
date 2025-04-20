17decdd41f7307fd3c12e83f5faba1d4e8a35a46
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2025-04-20 09:52:23 +0800

    made the select values title

--- app/Traits/HasEnumOptions.php
+++ app/Traits/HasEnumOptions.php
@@ -2,11 +2,16 @@
 
 namespace App\Traits;
 
+use Illuminate\Support\Str;
+
 trait HasEnumOptions
 {
     public static function options()
     {
         $data = array_column(self::cases(), 'value');
-        return array_combine($data, $data);
+        $values = collect($data)->map(function ($item) {
+            return Str::title($item);
+        });
+        return array_combine($data, $values->toArray());
     }
 }
